c46d846016ef8696b668f948ff3af9d2cb0f8ab1
ridwanali <ridwanali404@gmail.com>
2023-07-11 12:13:34 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/bottom_bar.dart';
+import 'package:flutter_application_1/utils/app_styles.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Styles.primaryColor,
+      ),
+      home: const BottomBar(),
+    );
+  }
+}
